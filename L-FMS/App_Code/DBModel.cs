855fabd5e9ca944f592b3d622e40f9c469ef259b
h1994st <h1994st@gmail.com>
2014-07-30 11:40:00 +0800

    add DBModel

--- L-FMS/App_Code/DBModel.cs
+++ L-FMS/App_Code/DBModel.cs
@@ -45,5 +45,93 @@ namespace L_FMS
             return result;
         }
 
+        // 注册新用户
+        public void RegisterNewUser(ACCOUNT account, USERINFO userinfo)
+        {
+            USER_USERINFO user_userinfo = new USER_USERINFO
+            {
+                ID = DBModel.GetInstance().GetSeqNextVal("user_userinfo"),
+                ACCOUNT = account.USER_ID,
+                USERINFO = userinfo.USERINFO_ID
+            };
+
+            using (LFMSContext db = new LFMSContext())
+            {
+                try
+                {
+                    db.ACCOUNT.Add(account);
+                    db.USERINFO.Add(userinfo);
+                    db.USER_USERINFO.Add(user_userinfo);
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine("Unique");
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public void RegisterNewUser(HttpRequest Request)
+        {
+            string email = Request.Form["email"];
+            string pwd = Request.Form["pwd"];
+            string pwdValidate = Request.Form["pwd-validate"];
+
+            if (pwd.Equals(pwdValidate))
+            {
+                pwd = MD5.Encrypt(pwd);
+            }
+
+            string name = Request.Form["user-name"];
+            string phone = Request.Form["phone"];
+            string address = Request.Form["address"];
+            string major = Request.Form["major"];
+            string sex = Request.Form["sex"];
+            string birth = Request.Form["birth"];
+
+            ACCOUNT account = new ACCOUNT
+            {
+                USER_ID = DBModel.GetInstance().GetSeqNextVal("user"),
+                EMAIL = email,
+                PASSWORD = pwd,
+                PRIVILEGE = 1,
+                VERIFIED = 1
+            };
+
+            USERINFO userinfo = new USERINFO
+            {
+                USERINFO_ID = DBModel.GetInstance().GetSeqNextVal("userinfo"),
+                USER_NAME = name,
+                PHONE = phone,
+                ADDRESS = address,
+                MARJOR = major,
+                SEX = (sex.Equals("0") ? "M" : "F"),
+                BIRTH = DateTime.ParseExact(birth, "yyyy-MM-dd", null)
+            };
+
+            USER_USERINFO user_userinfo = new USER_USERINFO
+            {
+                ID = DBModel.GetInstance().GetSeqNextVal("user_userinfo"),
+                ACCOUNT = account.USER_ID,
+                USERINFO = userinfo.USERINFO_ID
+            };
+
+            using (LFMSContext db = new LFMSContext())
+            {
+                try
+                {
+                    db.ACCOUNT.Add(account);
+                    db.USERINFO.Add(userinfo);
+                    db.USER_USERINFO.Add(user_userinfo);
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine("Unique");
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+            }
+        }
     }
 }
--- L-FMS/Register.aspx.cs
+++ L-FMS/Register.aspx.cs
@@ -18,71 +18,9 @@ namespace L_FMS
 
         protected void Create_User(object sender, EventArgs e)
         {
+            DBModel.GetInstance().RegisterNewUser(Request);
 
-            string email = Request.Form["email"];
-            string pwd = Request.Form["pwd"];
-            string pwdValidate = Request.Form["pwd-validate"];
-
-            if (pwd.Equals(pwdValidate))
-            {
-                pwd = MD5.Encrypt(pwd);
-            }
-
-            string name = Request.Form["user-name"];
-            string phone = Request.Form["phone"];
-            string address = Request.Form["address"];
-            string major = Request.Form["major"];
-            string sex = Request.Form["sex"];
-            string birth = Request.Form["birth"];
-
-            using (LFMSContext db = new LFMSContext())
-            {
-
-                try
-                {
-                    ACCOUNT account = new ACCOUNT
-                    {
-                        USER_ID = DBModel.GetInstance().GetSeqNextVal("user"),
-                        EMAIL = email,
-                        PASSWORD = pwd,
-                        PRIVILEGE = 1,
-                        VERIFIED = 1
-                    };
-
-                    USERINFO userinfo = new USERINFO
-                    {
-                        USERINFO_ID = DBModel.GetInstance().GetSeqNextVal("userinfo"),
-                        USER_NAME = name,
-                        PHONE = phone,
-                        ADDRESS = address,
-                        MARJOR = major,
-                        SEX = (sex.Equals("0") ? "M" : "F"),
-                        BIRTH = DateTime.ParseExact(birth, "yyyy-MM-dd", null)
-                    };
-
-                    USER_USERINFO user_userinfo = new USER_USERINFO
-                    {
-                        ID = DBModel.GetInstance().GetSeqNextVal("user_userinfo"),
-                        ACCOUNT = account.USER_ID,
-                        USERINFO = userinfo.USERINFO_ID
-                    };
-
-                    db.ACCOUNT.Add(account);
-                    db.USERINFO.Add(userinfo);
-                    db.USER_USERINFO.Add(user_userinfo);
-                    db.SaveChanges();
-
-
-
-                    // 跳转回主页
-                    Response.Redirect("~/");
-                }
-                catch (Exception ex)
-                {
-                    System.Diagnostics.Debug.WriteLine("Unique");
-                    System.Diagnostics.Debug.WriteLine(ex.Message);
-                }
-            }
+            Response.Redirect("~/");
         }
     }
 }

6abb599378268f50c972d25c1d3e7fd04d87a526
o0lazybear0o <o0lazybear0o@gmail.com>
2014-07-31 10:59:28 +0800

    usertable 添加,DBmodel 有更改

--- L-FMS/SettingQuestions.aspx.cs
+++ L-FMS/SettingQuestions.aspx.cs
@@ -10,10 +10,11 @@ namespace L_FMS
     public partial class SettingQuestions : System.Web.UI.Page
     {
 
-        protected QUESTION[] questions;
+        protected QUESTION[] questions = new QUESTION[3];
         protected void Page_Load(object sender, EventArgs e)
         {
-            questions = DBModel.GetInstance()
+            for (int i = 0; i < 3; ++i)
+                questions[i] = DBModel.GetInstance().GetQustion(i + 1);
         }
     }
 }
